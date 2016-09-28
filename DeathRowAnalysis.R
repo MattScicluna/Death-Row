e1a1d2bf788bfa13496df3eb840d001e550a873b
Matthew Scicluna <mattcscicluna@gmail.com>
2016-09-28 19:08:01 -0400

    Add files via upload

---
+++ DeathRowAnalysis.R
@@ -0,0 +1,89 @@
+#Death row inmates example
+library(tm)
+library(lsa)
+library(tsne)
+data=read.csv("C:\\Users\\Matthew Scicluna\\Desktop\\Files\\TA work\\STA313 TA\\Assignment1\\Death Row Data.csv")
+dataset=DataframeSource(data[1])
+mycorpus<-Corpus(dataset, readerControl=list(language="eng", reader=readPlain))
+mycorpus <- tm_map(mycorpus, removePunctuation)
+mycorpus <- tm_map(mycorpus, removeNumbers)
+mycorpus <- tm_map(mycorpus, stemDocument)
+stopwordseng=stopwords(kind = "en")
+mycorpus <- tm_map(mycorpus, removeWords, stopwordseng)
+mycorpus <- tm_map(mycorpus, content_transformer(tolower))
+
+#getTransformations() to see your options!!!
+TDM=TermDocumentMatrix(mycorpus)
+TDMred=removeSparseTerms(TDM, 0.9)
+findFreqTerms(TDM, 30)
+findAssocs(TDM, "jesus", 0.3)
+
+
+TDMMat<-as.matrix(TDMred)
+head(TDMMat[,1:2]) #sees beginning of first 2 documents only
+
+
+LSAOBJ=lsa(TDMMat)
+TK=as.matrix(as.data.frame(LSAOBJ[1]))
+DK=as.matrix(as.data.frame(LSAOBJ[2]))
+SK=as.matrix(as.data.frame(LSAOBJ[3]))
+
+#make SK diagonal matrix
+SK=diag(SK[,1])
+
+dim(TK) #Note the dimensions!
+#[1] 53 13
+dim(SK)
+#[1] 13  13
+dim(DK)
+#[1] 495  13
+dim(TDM2)
+#[1]  53 495
+
+MK=TK%*%SK%*%t(DK)
+#compare the reconstruction
+TDMred=as.matrix(TDMred)
+head(as.matrix(TDMred))
+head(MK)
+
+#Now lets reconstruct document 1 using this
+recon=solve(SK)%*%t(TK)%*%TDMred[,1]
+recon2=solve(SK)%*%t(TK)%*%TDMred[,2]
+#compare cosine of original docs and reconstruction
+cosine(as.vector(recon),as.vector(recon2))
+cosine(as.vector(TDMred[,1]),as.vector(TDMred[,2]))
+cosine(as.vector(MK[,1]),as.vector(MK[,2]))
+
+#Reduced the dimensionality from 53 to 13.
+recon=solve(SK)%*%t(TK)%*%TDMred
+
+KMEANSTEST=kmeans(t(recon),2)
+data=as.vector(data)
+racecluster1=data[,7][unlist(KMEANSTEST[1])==1]
+racecluster2=data[,7][unlist(KMEANSTEST[1])==2]
+
+summary(racecluster1) #Is race causing this cluster?
+summary(racecluster2)
+
+countycluster1=data[,8][unlist(KMEANSTEST[1])==1]
+countycluster2=data[,8][unlist(KMEANSTEST[1])==2]
+
+summary(countycluster1) #Is county affecting this cluster?
+summary(countycluster2)
+
+year=substr(data[,6],7,10) #Gets the year
+yearcluster1=year[unlist(KMEANSTEST[1])==1]
+yearcluster2=year[unlist(KMEANSTEST[1])==2]
+summary(factor(yearcluster1))
+summary(factor(yearcluster2))
+
+
+TDMC1=TDMMat[,unlist(KMEANSTEST[1])==1]
+TDMC2=TDMMat[,unlist(KMEANSTEST[1])==2]
+
+
+#Another cool way to cluster the data!
+test=tsne(t(TDMred),k=2)
+KMEANSTEST=kmeans(test,2)
+plot(test)
+points(as.matrix(as.data.frame(KMEANSTEST[2])),col='red')
---
+++ deathrowinfo.py
@@ -0,0 +1,51 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import csv
+import time
+
+#get the webpage with the prisoner data
+uri='http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html'
+
+f = csv.writer(open("Death Row Data" + ".csv", "w", newline=''), dialect='excel')
+
+# Write column headers as the first line
+f.writerow(["Last Statement", "Last Name", "First Name", "TDCJ Num", "Age", "Date", "Race","County"])
+
+urllines = urllib.request.urlopen(uri)
+pagedat = urllines.read()
+urllines.close()
+soup = BeautifulSoup(pagedat)
+allrows = soup.find_all("tr")
+suburi = uri[:38]
+for row in allrows:
+    tds = row.find_all("td")
+    try:
+        links=row.find_all('a')
+        link=links[1].get('href')
+        LSLink = suburi+link
+        urllines2 = urllib.request.urlopen(LSLink)
+        pagedat2 = urllines2.read()
+        urllines2.close()
+        soup2 = BeautifulSoup(pagedat2)
+        par = soup2.find_all("p")
+        for i in range(1,(len(par)-1)):
+            if 'Last Statement:' in par[i].get_text():
+                LS=str(par[i+1].get_text())
+                LS=LS.replace('\x92s','')
+                LS=LS.replace('\xa0','')
+                
+        LN = str(tds[3].get_text())
+        FN = str(tds[4].get_text())
+        TDCJ = str(tds[5].get_text())
+        Age = str(tds[6].get_text())
+        Date = str(tds[7].get_text())
+        Race = str(tds[8].get_text())
+        County = str(tds[9].get_text())
+        print("good string")
+    except Exception:
+        print("bad string")
+        time.sleep(.1)
+        continue
+    #print [LS, LN, FN, TDCJ, Age, Date, Race, County]
+    f.writerow([LS, LN, FN, TDCJ, Age, Date, Race, County])
+    LS=''
