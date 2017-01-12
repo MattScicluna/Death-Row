3968528109a2ae24e602a291f5c3ffb624d27b7b
MattScicluna <mattcscicluna@gmail.com>
2017-01-12 12:08:16 -0500

    imporved R script

--- DeathRowAnalysis.R
+++ DeathRowAnalysis.R
@@ -1,8 +1,20 @@
-#Death row inmates example
-library(tm)
-library(lsa)
-library(tsne)
-data=read.csv("C:\\Users\\Matthew Scicluna\\Desktop\\Files\\TA work\\STA313 TA\\Assignment1\\Death Row Data.csv")
+#Death row inmates R analysis
+
+
+#Install necessary packages
+pckgs = list("tm", "lsa", "tsne")
+
+func <- function(x){
+  if(!is.element(x, rownames(installed.packages())))
+  {install.packages(x)}
+  }
+
+lapply(pckgs, func)
+lapply(pckgs, library, character.only=TRUE)
+
+
+#Read in the data
+data=read.csv(".\\Death Row Data.csv")
 dataset=DataframeSource(data[1])
 mycorpus<-Corpus(dataset, readerControl=list(language="eng", reader=readPlain))
 mycorpus <- tm_map(mycorpus, removePunctuation)
@@ -11,6 +23,7 @@ mycorpus <- tm_map(mycorpus, stemDocument)
 stopwordseng=stopwords(kind = "en")
 mycorpus <- tm_map(mycorpus, removeWords, stopwordseng)
 mycorpus <- tm_map(mycorpus, content_transformer(tolower))
+
 
 #getTransformations() to see your options!!!
 TDM=TermDocumentMatrix(mycorpus)
@@ -23,6 +36,7 @@ TDMMat<-as.matrix(TDMred)
 head(TDMMat[,1:2]) #sees beginning of first 2 documents only
 
 
+#Perform Latent Semantic Analysis
 LSAOBJ=lsa(TDMMat)
 TK=as.matrix(as.data.frame(LSAOBJ[1]))
 DK=as.matrix(as.data.frame(LSAOBJ[2]))
@@ -41,22 +55,29 @@ dim(TDM2)
 #[1]  53 495
 
 MK=TK%*%SK%*%t(DK)
+
 #compare the reconstruction
 TDMred=as.matrix(TDMred)
 head(as.matrix(TDMred))
 head(MK)
 
+
 #Now lets reconstruct document 1 using this
 recon=solve(SK)%*%t(TK)%*%TDMred[,1]
 recon2=solve(SK)%*%t(TK)%*%TDMred[,2]
+
+
 #compare cosine of original docs and reconstruction
 cosine(as.vector(recon),as.vector(recon2))
 cosine(as.vector(TDMred[,1]),as.vector(TDMred[,2]))
 cosine(as.vector(MK[,1]),as.vector(MK[,2]))
 
+
 #Reduced the dimensionality from 53 to 13.
 recon=solve(SK)%*%t(TK)%*%TDMred
 
+
+#Doing K means test on the reconstruction
 KMEANSTEST=kmeans(t(recon),2)
 data=as.vector(data)
 racecluster1=data[,7][unlist(KMEANSTEST[1])==1]
@@ -82,7 +103,7 @@ TDMC1=TDMMat[,unlist(KMEANSTEST[1])==1]
 TDMC2=TDMMat[,unlist(KMEANSTEST[1])==2]
 
 
-#Another cool way to cluster the data!
+#Another cool way to cluster the data using tsne!
 test=tsne(t(TDMred),k=2)
 KMEANSTEST=kmeans(test,2)
 plot(test)
